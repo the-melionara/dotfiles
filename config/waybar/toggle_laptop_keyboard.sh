b753aa26c5b5e430527bdc0fb65e23566d383e1b
Mel <yowax25yt@gmail.com>
2025-05-11 22:00:30 +0200

    added hyprnotify and shit

--- config/waybar/toggle_laptop_keyboard.sh
+++ config/waybar/toggle_laptop_keyboard.sh
@@ -4,8 +4,10 @@ CACHE_FILE="/home/mel/.cache/laptop-keyboard-disabled"
 
 if [ -f "$CACHE_FILE" ] ;then
     rm "$CACHE_FILE"
+    notify-send -u low -t 2000 "Laptop keyboard enabled"
     sh -c "hyprctl keyword 'device[at-translated-set-2-keyboard]:enabled' 1"
 else
     touch "$CACHE_FILE"
+    notify-send -u low -t 2000 "Laptop keyboard disabled"
     sh -c "hyprctl keyword 'device[at-translated-set-2-keyboard]:enabled' 0"
 fi
